f4f75e8d795b5338dbedc68ab781a50dc9898990
Dennis Fuchs <fudennis@gmx.de>
2022-02-27 15:34:07 +0100

    Use lambda

--- orjson_benchmark/benchmark_latency.py
+++ orjson_benchmark/benchmark_latency.py
@@ -1,24 +1,44 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import json
 from json import loads as json_loads
 
+import orjson
 import pytest
+import rapidjson
+import simplejson
+import ujson
 
 from .data import fixtures
-from .json_libraries import libraries
+from .json_libraries import LIBRARIES, get_version
 from .util import read_fixture_str
 
 
-@pytest.mark.parametrize("library", libraries)
+def get_dumper_loader(library: str):
+    # dumps wrappers that return UTF-8
+    if library == "json":
+        return lambda data: json.dumps(data).encode("utf-8"), json.loads
+    elif library == "simplejson":
+        return lambda data: simplejson.dumps(data).encode("utf-8"), simplejson.loads
+    elif library == "ujson":
+        return lambda data: ujson.dumps(data).encode("utf-8"), ujson.loads
+    elif library == "rapidjson":
+        return lambda data: rapidjson.dumps(data).encode("utf-8"), rapidjson.loads
+    elif library == "orjson":
+        return lambda data: orjson.dumps(data), orjson.loads
+    else:
+        raise NotImplementedError
+
+
+@pytest.mark.parametrize("library", LIBRARIES)
 @pytest.mark.parametrize("fixture", fixtures)
 def test_dumps(benchmark, fixture, library):
-    dumper, _, version = libraries[library]
-
+    dumper, _ = get_dumper_loader(library)
     benchmark.group = f"{fixture} serialization"
-    benchmark.name = f"{library} {version}"
+    benchmark.name = f"{library} {get_version(library)}"
     benchmark.extra_info["lib"] = library
-    benchmark.extra_info["version"] = version
+    benchmark.extra_info["version"] = get_version(library)
 
     data = read_fixture_str(f"{fixture}.xz")
     if not json_loads(dumper(data)) == data:
@@ -28,14 +48,14 @@ def test_dumps(benchmark, fixture, library):
 
 
 @pytest.mark.parametrize("data", ["[]", "{}", '""'])
-@pytest.mark.parametrize("library", libraries)
+@pytest.mark.parametrize("library", LIBRARIES)
 def test_empty(benchmark, data, library):
-    dumper, loader, version = libraries[library]
+    dumper, loader = get_dumper_loader(library)
 
     benchmark.group = f"empty deserialization"
-    benchmark.name = f"{library} {version} {data}"
+    benchmark.name = f"{library} {get_version(library)} {data}"
     benchmark.extra_info["lib"] = library
-    benchmark.extra_info["version"] = version
+    benchmark.extra_info["version"] = get_version(library)
 
     if not json_loads(dumper(loader(data))) == json_loads(data):
         assert False
@@ -44,14 +64,14 @@ def test_empty(benchmark, data, library):
 
 
 @pytest.mark.parametrize("fixture", fixtures)
-@pytest.mark.parametrize("library", libraries)
+@pytest.mark.parametrize("library", LIBRARIES)
 def test_loads(benchmark, fixture, library):
-    dumper, loader, version = libraries[library]
+    dumper, loader = get_dumper_loader(library)
 
     benchmark.group = f"{fixture} deserialization"
-    benchmark.name = f"{library} {version}"
+    benchmark.name = f"{library} {get_version(library)}"
     benchmark.extra_info["lib"] = library
-    benchmark.extra_info["version"] = version
+    benchmark.extra_info["version"] = get_version(library)
 
     data = read_fixture_str(f"{fixture}.xz")
 
